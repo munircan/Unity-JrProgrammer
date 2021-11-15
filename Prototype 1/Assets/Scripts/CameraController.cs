0edb7018c72a18cfe9ee1b701829a48aa1fc78e0
Münir Can <52739121+munircan@users.noreply.github.com>
2021-11-15 22:13:20 +0300

    Update Player Controller

--- Prototype 1/Assets/Scripts/CameraController.cs
+++ Prototype 1/Assets/Scripts/CameraController.cs
@@ -7,9 +7,11 @@ public class CameraController : MonoBehaviour
 {
    [SerializeField] private GameObject player;
    [SerializeField] private Vector3 offset;
+   
 
    private void LateUpdate()
    {
+      // Follow the Player with an offset
       transform.position = player.transform.position + offset;
    }
 }
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float forwardInput;
     private void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
+        //Rotate the vehicle sideways
+        transform.Rotate(Vector3.up,Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
