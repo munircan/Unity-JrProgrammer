280d95a99588a61529f9d24d5b86cf349d7685a2
Münir Can <52739121+munircan@users.noreply.github.com>
2021-11-23 00:27:38 +0300

    Remove the DestroyOutOfBounds.cs

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -6,9 +6,17 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private PlayerController playerController;
+    private float bound = -10;
+
+    private void Awake()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
 
     private void Update()
     {
-        transform.Translate(Vector3.left * (Time.deltaTime * speed));
+        if (!playerController.isGameOver) transform.Translate(Vector3.left * (Time.deltaTime * speed));
+        if (transform.position.x < bound && gameObject.CompareTag("Obstacle")) Destroy(gameObject);
     }
 }
