02310dbc9892ee3c98ce58a29f6890a5fe847a40
Münir Can <52739121+munircan@users.noreply.github.com>
2021-11-23 15:36:56 +0300

    Animation control

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -10,10 +10,12 @@ public class PlayerController : MonoBehaviour
     private float gravityModifier = 2.0f;
     private bool isOnGround = true;
     public bool isGameOver = false;
+    private Animator playerAnim;
 
     private void Awake()
     {
         _playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
     }
 
     private void Start()
@@ -23,16 +25,22 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !isGameOver)
         {
             _playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Ground")) isOnGround = true;
-        else if (other.gameObject.CompareTag("Obstacle")) isGameOver = true;
+        else if (other.gameObject.CompareTag("Obstacle"))
+        {
+            isGameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
     }
 }
