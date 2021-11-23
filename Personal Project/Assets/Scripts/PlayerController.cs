30dc16e086421fd51c753e4535174eb4052d6e9a
Münir Can <52739121+munircan@users.noreply.github.com>
2021-11-23 19:28:52 +0300

    Updated Project

---
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody playerRb;
+    private float boundZ = 10;
+
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+        ConstrainPlayerPosition();
+    }
+
+    private void PlayerMovement()
+    {
+        var direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        playerRb.velocity = speed * direction;
+    }
+
+    private void ConstrainPlayerPosition()
+    {
+        if (transform.position.z > boundZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, boundZ);
+        }
+
+        if (transform.position.z < -boundZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -boundZ);
+        }
+    }
+}
