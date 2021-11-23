cdf3aa3ae1430b357ba70de9ee26e16603792e9f
Münir Can <52739121+munircan@users.noreply.github.com>
2021-11-23 16:12:37 +0300

    Add Audio

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudioSource;
+    [SerializeField] private ParticleSystem explosionParticleSystem;
+    [SerializeField] private ParticleSystem dirtParticleSystem;
+    [SerializeField] private AudioClip crashSound;
+    [SerializeField] private AudioClip jumpsSound;
     private float jumpForce = 12.5f;
     private float gravityModifier = 2.0f;
     private bool isOnGround = true;
     public bool isGameOver = false;
-    private Animator playerAnim;
+    
 
     private void Awake()
     {
         _playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudioSource = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -30,17 +37,26 @@ public class PlayerController : MonoBehaviour
             _playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticleSystem.Stop();
+            playerAudioSource.PlayOneShot(jumpsSound);
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Ground")) isOnGround = true;
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirtParticleSystem.Play();
+        }
         else if (other.gameObject.CompareTag("Obstacle"))
         {
             isGameOver = true;
+            explosionParticleSystem.Play();
+            dirtParticleSystem.Stop();
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            playerAudioSource.PlayOneShot(crashSound);
         }
     }
 }
