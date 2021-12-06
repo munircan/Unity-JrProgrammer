322a073f56cc0ade55c7cf358116cff6ded5ee2e
Münir Can <52739121+munircan@users.noreply.github.com>
2021-12-07 02:14:58 +0300

    Optimizations

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float horsePower;
     [SerializeField] private float turnSpeed;
     [SerializeField] private float horizontalInput;
     [SerializeField] private float forwardInput;
@@ -13,26 +16,57 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Camera mainCamera;
     [SerializeField] private Camera secondCamera;
     [SerializeField] private KeyCode switchKey = KeyCode.Space;
-    
-    
-    
-    private void Update()
+    [SerializeField] private GameObject centerOfMass;
+    [SerializeField] private TextMeshProUGUI speedometer;
+    [SerializeField] private TextMeshProUGUI rpmText;
+    private float rpm;
+    private Rigidbody playerRb;
+
+    [SerializeField] private List<WheelCollider> wheels;
+    private int wheelCount;
+
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
+
+    private void FixedUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        
-        //Move the vehicle forward
-        transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
-        //Rotate the vehicle sideways
-        transform.Rotate(Vector3.up,Time.deltaTime * turnSpeed * horizontalInput);
-        
-        if(Input.GetKeyDown(switchKey))
+
+        if (IsOnGround())
         {
-            mainCamera.enabled = !mainCamera.enabled;
-            secondCamera.enabled = !secondCamera.enabled;
+            //Move the vehicle forward
+            //transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
+            playerRb.AddRelativeForce(Vector3.forward * (horsePower * forwardInput));
+            //Rotate the vehicle sideways
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+
+            speed = playerRb.velocity.magnitude * 3.6f;
+            speedometer.text = "Speed: " + (int) speed + "kph";
+            rpm = (speed % 30) * 40;
+            rpmText.text = "RPM: " + (int) rpm;
         }
 
-        
-        
+
+        if (!Input.GetKeyDown(switchKey)) return;
+        mainCamera.enabled = !mainCamera.enabled;
+        secondCamera.enabled = !secondCamera.enabled;
+    }
+
+
+    private bool IsOnGround()
+    {
+        wheelCount = 0;
+        foreach (var wheel in wheels.Where(wheel => wheel.isGrounded))
+        {
+            wheelCount++;
+        }
+
+        return wheelCount.Equals(4);
     }
 }
--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -1,33 +1,49 @@
-using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private float horizontalInput;
-    [SerializeField] private float speed;
-    [SerializeField] private float range = 10;
-    [SerializeField] private GameObject foodPrefab;
+    private float speed = 20.0f;
+    private float xRange = 20;
+    public GameObject projectilePrefab;
 
-    private void Update()
+
+    // Update is called once per frame
+    void Update()
     {
-        if (transform.position.x < -range)
+        // Check for left and right bounds
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-range, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x > range)
+        if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(range, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        // Player movement left to right
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(foodPrefab, transform.position, foodPrefab.transform.rotation);
+            // No longer necessary to Instantiate prefabs
+            // Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+            // Get an object object from the pool
+            GameObject pooledProjectile = ObjectPooler.SharedInstance.GetPooledObject();
+            if (pooledProjectile != null)
+            {
+                pooledProjectile.SetActive(true); // activate it
+                pooledProjectile.transform.position = transform.position; // position it at player
+            }
         }
+
+
+
     }
 }
