67c53421559179d115879a9e651ad2090a104887
Münir Can <52739121+munircan@users.noreply.github.com>
2021-12-07 16:41:10 +0300

    Pachinko

---
+++ Counting Prototype/Assets/Scripts/Counter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    [FormerlySerializedAs("CounterText")] [SerializeField]
+    private Text counterText;
+
+    private int _count = 0;
+    [SerializeField] private int point;
+    [SerializeField] private GameObject spherePrefab;
+
+    private void Start()
+    {
+        _count = 0;
+        StartCoroutine(SphereMaker());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _count += point;
+        counterText.text = "Count : " + _count;
+    }
+
+    private int GetCount()
+    {
+        return _count;
+    }
+    private IEnumerator SphereMaker()
+    {
+        while (GetCount() < 100)
+        {
+            yield return new WaitForSeconds(5);
+            Instantiate(spherePrefab);
+        }
+        
+    }
+}
